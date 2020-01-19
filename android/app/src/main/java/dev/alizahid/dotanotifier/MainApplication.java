83d6176e996df7c5323ffd3bfb590e69db71f101
Ali Zahid <ali.zahid@live.com>
2020-01-20 02:45:41 +0400

    Notification channels added

--- android/app/src/main/java/dev/alizahid/dotanotifier/MainApplication.java
+++ android/app/src/main/java/dev/alizahid/dotanotifier/MainApplication.java
@@ -1,6 +1,12 @@
 package dev.alizahid.dotanotifier;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.AudioAttributes;
+import android.net.Uri;
+import android.os.Build;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
@@ -43,5 +49,25 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
 
     SoLoader.init(this, false);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+      if (notificationManager != null) {
+        NotificationChannel readyCheck = new NotificationChannel("ready_check", "ready_check", NotificationManager.IMPORTANCE_HIGH);
+        NotificationChannel matchReady = new NotificationChannel("match_ready", "match_ready", NotificationManager.IMPORTANCE_HIGH);
+
+        AudioAttributes attributes = new AudioAttributes.Builder()
+          .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+          .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+          .build();
+
+        readyCheck.setSound(Uri.parse((getResources().getResourceEntryName(R.raw.ready_check_no_focus))), attributes);
+        matchReady.setSound(Uri.parse((getResources().getResourceEntryName(R.raw.match_ready_no_focus))), attributes);
+
+        notificationManager.createNotificationChannel(readyCheck);
+        notificationManager.createNotificationChannel(matchReady);
+      }
+    }
   }
 }
